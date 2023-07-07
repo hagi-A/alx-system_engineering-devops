9fa54f4d4a80e48be0629f1f6b87fd6f021453db
Hareg <haregalemu9@gmail.com>
2023-07-07 14:34:25 -0700

    advanced

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,7 @@
 
 file_line { 'Declare identity file':
   path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/holberton',
+  line => 'IdentityFile ~/.ssh/school',
 }
 
 file_line { 'Turn off passwd auth':
